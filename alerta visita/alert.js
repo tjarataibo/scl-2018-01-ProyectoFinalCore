b7c04d478def705e382b4d13900efa1cecd84c9b
Tamara Hinojosa Salinas <tamara.hi.sa@hotmail.com>
2018-07-29 23:45:12 -0400

    Se crea carpeta de alerta visita y se da funcionalidad

---
+++ alerta visita/alert.js
@@ -0,0 +1,31 @@
+let button = document.getElementById("button");
+
+button.addEventListener("click", function(){
+    notify();
+});
+
+function notify(){
+let properties = {
+    body: "Tienes un nue vo mensaje",
+    icon: "icononotificacionesj1.png"
+}
+
+    // verificar que el navegador soporta notificaciones
+    if (!("Notification" in window)) {
+        alert("Tu navegadir no soporta notificaciones");
+
+        //Lanzar notificacion si ya esta autorizado el servicio
+    }else if(Notification.permission !== "denied"){
+        Notification.requestPermission(function(permission){
+           if(Notification.permission === "granted"){
+               let notificacion = new Notification("Tienes una nueva visita, ¿deseas recibirla?", properties);
+               notificacion.onclick = (ev)=>{
+                   ev.preventDefault();
+                   window.open(`https://www.instagram.com`);
+               }
+           }
+        });
+    }
+
+}
+
