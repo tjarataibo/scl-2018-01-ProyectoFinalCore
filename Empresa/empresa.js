35d36009d680b6b9e9c553a503b962cc3e9c2427
Tamara <tjarataibo@gmail.com>
2018-07-30 14:39:44 -0400

    Adhesión de login a empresa

---
+++ Empresa/empresa.js
@@ -0,0 +1,23 @@
+let formulario = document.getElementById('formulario');
+// console.log(formulario);
+// manejar algo quiere decir que controlar lo que contiene
+// el evento es un momento
+function manejarSubmit(evento) {
+    // preventDefault() detiene el evento, en este caso el envio no ocurre.
+    evento.preventDefault();
+    const correo = document.getElementById('correoElectronico').value;
+    const contrasena = document.getElementById('contraseña').value;
+    if(correo.length != 0 && contrasena.length != 0) {
+        firebase.auth().signInWithEmailAndPassword(correo, contrasena)
+        .then((respondse)=> {
+          location.href ="../despedida/despedida.html";
+        })
+        .catch(function(error) {
+            let errorCode = error.code;
+            alert(errorCode);
+        });
+    };
+}
+// se ejecuta la funcion
+formulario.addEventListener('submit', manejarSubmit);
+
