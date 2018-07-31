6dd79565f517d007763c5ce022714f7b9bcce3a7
Alicia Centeno <alicent.08.02@gmail.com>
2018-07-30 23:11:23 -0400

    Se guarda el registro en database con exito

--- Registro/registro.js
+++ Registro/registro.js
@@ -1,64 +1,75 @@
 //Registro
 
-/* var db = firebase.firestore();
-const formulario = document.getElementById('formulario');
+const dataList = document.querySelector('#data-list');
+const form = document.querySelector('#formulario');
 
-formulario.addEventListener('submit', e =>  {
+// se crea elemntos del Dom
+/*function renderData(doc) {
+    let li = document.createElement('li');
+    let name = document.createElement('span');
+    let email = document.createElement('span');
+    let rut = document.createElement('span');
+    let number = document.createElement('span');
+    let patente = document.createElement('span');
+
+    // se agregan atributos
+
+    li.setAttribute('data-id', doc.id);
+    name.textContent = doc.data().nombre;
+    email.textContent = doc.data().Email;
+    rut.textContent = doc.data().Rut;
+    number.textContent = doc.data().Telefono;
+    patente.textContent = doc.data().patente;
+
+    // se unen los hijos a los padres
+
+    li.appendChild(name);
+    li.appendChild(email);
+    li.appendChild(rut);
+    li.appendChild(number);
+    li.appendChild(patente);
+    dataList.appendChild(li);
+
+
+}*/
+
+
+//codigo que hace posible recibir la data 
+db.collection('proyecto final').get().then((snapshot) => {
+    //console.log(snapshot.docs);
+    snapshot.docs.forEach(doc => {
+        console.log(doc.data());
+        //renderData();
+    });
+})
+
+// codigo que guarda data en database
+form.addEventListener('submit', (e) => {
     e.preventDefault();
-    const nombreApellido = document.getElementById('nameLastname').value;
-    const rut = document.getElementById('di').value;
-    const telefono = document.getElementById('phone').value;
-    const correo = document.getElementById('correo').value;
-    const contrasena = document.getElementById('contrasena').value;
-    if(nombreApellido.length != 0 && rut.length != 0 && telefono.length != 0
-    && correo.length != 0 && contrasena.length != 0) {
-        console.log('Usuario ya existente')
-
-        //  else (nombreApellido.length != 0 && rut.length != 0 && telefono.length != 0
-        //         && correo.length != 0 && contrasena.length != 0) {
-        //     alert("Usuario ya existente");
-        //             var confirmar = confirm("Estás a punto de comenzar la trivia, ¿Quieres comenzar?.");
-        //             if (confirmar)
-        //             location.href ="../Despedida/despedida.html";;
+    db.collection('proyecto final').add({
+        nombre: form.nombre.value,
+        email: form.email.value,
+        rut: form.rut.value,
+        telefono: form.tlf.value,
+        patente: form.patente.value
 
 
-        firebase.auth().createUserWithEmailAndPassword(correo, contrasena)
-        .then(Response => {
-            const userId = Response.user.uid;
-            firebase.database().ref("users/" + userId).set( {
-               nombreApellido: nombreApellido,
-               rut: rut,
-               telefono: telefono,
-               correo: correo,
-               contrasena: contrasena 
-            })
-            console.log('Registro exitoso');
-            location.href ="../Alerta/alerta.html";
-        })
-       // location.href = '../login/login.html';
-    }
-}); */
+    })
+    // para mantener limpio los input luego de haber enviado los datos
+    form.nombre.value = '';
+    form.email.value = '';
+    form.rut.value = '';
+    form.tlf.value = '';
+    form.patente.value = '';
+})
+
+
 
 function paraIrALogin() {
     location.href = '../login/login.html';
 }
 
 
-//codigo que hace posible ver la data en la consola y llevar a objeto
-db.collection('proyecto final').get().then((snapshot) => {
-    //console.log(snapshot.docs);
-    snapshot.docs.forEach(doc => {
-        console.log(doc.data())
-    
-    });
-})
-
-/*function getTime() {
-let fecha = new Date(); 
-let fecha2 = fecha.getDay()+" "+fecha.getDate()+" /"+fecha.getMonth()+" /"+fecha.getFullYear()+" "+fecha.getHours()+":"+fecha.getMinutes();
-return(fecha2);
-
-}*/
 
 
 
