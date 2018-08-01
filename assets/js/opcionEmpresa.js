572c0e5f6f46fa5cab3f5877442c9299aac6e4a5
Tamara <tjarataibo@gmail.com>
2018-07-31 20:09:42 -0400

    Estructura de registro e ingreso a empresa listo

---
+++ assets/js/opcionEmpresa.js
@@ -0,0 +1,10 @@
+//Funcionalidad Parallax
+$(document).ready(function(){
+    $('.parallax').parallax();
+  });
+
+//Funcionalidad ayuda
+// $(document).ready(function(){
+//   $('.tap-target').tapTarget();
+// });
+        
